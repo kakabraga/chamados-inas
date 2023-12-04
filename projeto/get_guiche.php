4c41060228f29dffec5b94c751e5a956fd01ef19
JotaDF <j.wilson.df@gmail.com>
2023-12-04 16:46:32 -0300

    guichê

---
+++ projeto/get_guiche.php
@@ -0,0 +1,22 @@
+<?php
+	include_once('actions/ManterGuiche.php'); 
+    include_once('actions/ManterUsuario.php');
+	
+	$manterGuiche = new ManterGuiche();
+    $manterUsuario = new ManterUsuario();
+	
+	$lista = $manterGuiche->listar();
+        
+        foreach ($lista as $obj) {
+            echo "<tr>";
+            echo "  <td>".$obj->id."</td>";
+            echo "  <td>".$obj->numero."</td>";
+            echo "  <td>".$manterUsuario->getSetorPorId($obj->id_usuario)->nome."</td>";
+            if($obj->excluir){
+                echo "  <td align='center'><button class='btn btn-primary btn-sm' type='button' onclick='alterar(".$obj->id.",\"".$obj->numero."\",\"".$obj->id_usuario."\")'><i class='fas fa-edit'></i></button>&nbsp;&nbsp;<button class='btn btn-danger btn-sm' type='button' onclick='excluir(".$obj->id.",\"".$obj->numero."\")'><i class='far fa-trash-alt'></i></button></td>";
+            } else {
+                echo "  <td align='center'><button class='btn btn-primary btn-sm' type='button' onclick='alterar(".$obj->id.",\"".$obj->numero."\",\"".$obj->id_usuario."\")'><i class='fas fa-edit'></i></button>&nbsp;&nbsp;<button class='btn btn-secondary btn-sm' type='button' title='Possuí dependências!'><i class='far fa-trash-alt' alt='Possuí dependências!'></i></button></td>";                
+            }
+            echo "</tr>";
+        }
+
