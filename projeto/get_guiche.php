4a96641c23815908fd7336d97b0985d5da75fd44
JotaDF <j.wilson.df@gmail.com>
2023-12-04 17:22:15 -0300

    Update get_guiche.php

--- projeto/get_guiche.php
+++ projeto/get_guiche.php
@@ -13,9 +13,9 @@
             echo "  <td>".$obj->numero."</td>";
             echo "  <td>".$manterUsuario->getUsuarioPorId($obj->usuario)->nome."</td>";
             if($obj->excluir){
-                echo "  <td align='center'><button class='btn btn-primary btn-sm' type='button' onclick='alterar(".$obj->id.",\"".$obj->numero."\",\"".$obj->id_usuario."\")'><i class='fas fa-edit'></i></button>&nbsp;&nbsp;<button class='btn btn-danger btn-sm' type='button' onclick='excluir(".$obj->id.",\"".$obj->numero."\")'><i class='far fa-trash-alt'></i></button></td>";
+                echo "  <td align='center'><button class='btn btn-primary btn-sm' type='button' onclick='alterar(".$obj->id.",\"".$obj->numero."\",\"".$obj->usuario."\")'><i class='fas fa-edit'></i></button>&nbsp;&nbsp;<button class='btn btn-danger btn-sm' type='button' onclick='excluir(".$obj->id.",\"".$obj->numero."\")'><i class='far fa-trash-alt'></i></button></td>";
             } else {
-                echo "  <td align='center'><button class='btn btn-primary btn-sm' type='button' onclick='alterar(".$obj->id.",\"".$obj->numero."\",\"".$obj->id_usuario."\")'><i class='fas fa-edit'></i></button>&nbsp;&nbsp;<button class='btn btn-secondary btn-sm' type='button' title='Possuí dependências!'><i class='far fa-trash-alt' alt='Possuí dependências!'></i></button></td>";                
+                echo "  <td align='center'><button class='btn btn-primary btn-sm' type='button' onclick='alterar(".$obj->id.",\"".$obj->numero."\",\"".$obj->usuario."\")'><i class='fas fa-edit'></i></button>&nbsp;&nbsp;<button class='btn btn-secondary btn-sm' type='button' title='Possuí dependências!'><i class='far fa-trash-alt' alt='Possuí dependências!'></i></button></td>";                
             }
             echo "</tr>";
         }
