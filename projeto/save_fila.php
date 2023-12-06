28eceadd059a4ba0086c61a719d314750a023a13
JotaDF <j.wilson.df@gmail.com>
2023-12-06 08:33:19 -0300

    Update save_fila.php

--- projeto/save_fila.php
+++ projeto/save_fila.php
@@ -9,10 +9,12 @@ $f = new Fila();
 $id = isset($_POST['id']) ? $_POST['id'] : 0;
 $nome = $_POST['nome'];
 $id_servico = $_POST['servico'];
+$preferencial = isset($_POST['preferencial']) ? $_POST['preferencial'] : 0;
 
 $f->id = $id;
 $f->nome = $nome;
 $f->servico = $id_servico;
+$f->preferencial = $preferencial;
 
 $db_fila->salvar($f);
 header('Location: filas.php');
