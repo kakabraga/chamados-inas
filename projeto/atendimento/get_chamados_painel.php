bab129501569f2b56d52af94b955854b9f7e85ae
JotaDF <j.wilson.df@gmail.com>
2023-12-11 09:44:59 -0300

    ok

--- projeto/atendimento/get_chamados_painel.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once('../actions/ManterFila.php');
-    $manterFila = new ManterFila();
-	
-	$lista = $manterFila->getChamadosPainel();
-
-    echo json_encode($lista);
--- projeto/atendimento/get_proximo_painel.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once('../actions/ManterFila.php');
-    $manterFila = new ManterFila();
-	
-	$lista = $manterFila->getProximoChamadoPainel();
-
-    echo json_encode($lista);
