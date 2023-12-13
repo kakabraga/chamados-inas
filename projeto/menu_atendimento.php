15bfb2a3631d515c9907064fa80f200ae3e24d46
JotaDF <j.wilson.df@gmail.com>
2023-12-13 14:13:30 -0300

    Create menu_atendimento.php

---
+++ projeto/menu_atendimento.php
@@ -0,0 +1,84 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+<!-- Sidebar - Brand -->
+<br/>
+<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+    <div class="sidebar-brand-icon mx-1"><img src="img/inas.svg" width="100%" /></div>
+</a>
+<br/>
+<!-- Divider -->
+<hr class="sidebar-divider">
+    <?php
+    if ($usuario_logado->perfil >= 1) {
+        ?>
+
+        <!-- Heading -->
+        <div class="sidebar-heading">
+            SISTEMAS
+        </div>
+        <?php
+        if ($usuario_logado->perfil == 1 || $usuario_logado->perfil == 8) {
+            ?>
+            <!-- Nav Item - Pages Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="servicos.php">
+                    <i class="fa fa-id-card"></i>
+                    <span>Gerenciar Serviços</span>
+                </a>
+            </li>
+            <!-- Nav Item - Pages Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="guiches.php">
+                    <i class="fa fa-laptop"></i>
+                    <span>Gerenciar Guichês</span>
+                </a>
+            </li>
+            <?php
+        }
+        ?>
+
+        <?php
+    }
+    ?>
+     <!-- Divider -->
+     <hr class="sidebar-divider">
+
+<!-- Nav Item - Pages Collapse Menu -->
+<?php
+    if ($usuario_logado->perfil <= 1 || $usuario_logado->perfil == 8 || $usuario_logado->perfil == 9) {
+        ?>
+            <!-- Nav Item - Pages Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="gerenciar_fila.php">
+                    <i class="fa fa-id-card"></i>
+                    <span>Gerenciar Fila</span>
+                </a>
+            </li>
+            <!-- Nav Item - Pages Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="gerenciar_atendimento.php">
+                    <i class="fa fa-laptop"></i>
+                    <span>Gerenciar atendimento</span>
+                </a>
+            </li>
+             <!-- Nav Item - Pages Collapse Menu -->
+             <li class="nav-item">
+                <a class="nav-link collapsed" target="_black" href="atendimento/painel.php">
+                    <i class="fa fa-users"></i>
+                    <span>Painel</span>
+                </a>
+            </li>
+<?php
+    }
+?>
+
+<!-- Divider -->
+<hr class="sidebar-divider">
+<!-- Sidebar Toggler (Sidebar) -->
+<div class="text-center d-none d-md-inline">
+    <button class="rounded-circle border-0" id="sidebarToggle"></button>
+</div>
+
+</ul>
+<!-- End of Sidebar -->
