f3e886d298b0369152011d67b0fc08994942200a
JotaDF <j.wilson.df@gmail.com>
2023-12-13 11:18:46 -0300

    Update menu.php

--- projeto/menu.php
+++ projeto/menu.php
@@ -9,36 +9,48 @@
 <br/>
 <!-- Divider -->
 <hr class="sidebar-divider">
+    <?php
+    if (isse($usuario_logado->perfil)) {
+        ?>
+
         <!-- Heading -->
+        <div class="sidebar-heading">
+            SISTEMAS
+        </div>
         <?php
         if ($usuario_logado->perfil <= 2) {
-            ?> 
-        <!-- Nav Item - Pages Collapse Menu -->
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="index_gerente.php">
-                <i class="fa fa-id-card"></i>
-                <span>GERENTE</span>
-            </a>
-        </li>    
-        <!-- Nav Item - Pages Collapse Menu -->
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="index_chamados.php">
-                <i class="fa fa-user"></i>
-                <span>CHAMADOS</span>
-            </a>
-        </li>
-        <?php
+            ?>        
+            <!-- Nav Item - Pages Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="index_gerente.php">
+                    <i class="fa fa-id-card"></i>
+                    <span>GERENTE</span>
+                </a>
+            </li>    
+            <!-- Nav Item - Pages Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="index_chamados.php">
+                    <i class="fa fa-user"></i>
+                    <span>CHAMADOS</span>
+                </a>
+            </li>
+            <?php
         }
         if ($usuario_logado->perfil == 1 || $usuario_logado->perfil == 8 || $usuario_logado->perfil == 9) {
-            ?> 
-        <li class="nav-item">
-            <a class="nav-link" href="gerenciar_fila.php">
-                <i class="fa fa-file" aria-hidden="true">
-                <span>ATENDIMENTO</span></a>
-        </li>
-<?php
+            ?>        
+            <!-- Nav Item - Pages Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link" href="gerenciar_fila.php">
+                    <i class="fa fa-file" aria-hidden="true">
+                    <span>ATENDIMENTO</span></a>
+            </li>
+            <?php
         }
-        ?> 
+        ?>        
+
+        <?php
+    }
+    ?>
     <!-- Divider -->
     <hr class="sidebar-divider">
 
