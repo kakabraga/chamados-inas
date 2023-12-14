92a401a2d9e2ed481df0824d3998d66579475067
JotaDF <j.wilson.df@gmail.com>
2023-12-14 11:17:01 -0300

    Update menu_gerente.php

--- projeto/menu_gerente.php
+++ projeto/menu_gerente.php
@@ -15,65 +15,35 @@
 
         <!-- Heading -->
         <div class="sidebar-heading">
-            ADMINISTRAÇÃO
+        Gestão de Tarefas
         </div>
         <?php
         if ($usuario_logado->perfil <= 1) {
             ?>        
-            <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="perfis.php">
-                    <i class="fa fa-id-card"></i>
-                    <span>Perfis</span>
-                </a>
-            </li>
-            <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="setores.php">
-                    <i class="fa fa-laptop"></i>
-                    <span>Setores</span>
-                </a>
-            </li>
-             <!-- Nav Item - Pages Collapse Menu -->
-             <li class="nav-item">
-                <a class="nav-link collapsed" href="equipes.php">
-                    <i class="fa fa-users"></i>
-                    <span>Equipes</span>
-                </a>
-            </li>
+        <!-- Nav Item - Pages Collapse Menu -->
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="equipes.php">
+                <i class="fa fa-users"></i>
+                <span>Equipes</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="tarefas.php">
+                <i class="fa fa-tasks"></i>
+                <span>Tarefas</span>
+            </a>
+        </li>
+        <!-- Nav Item - Pages Collapse Menu -->
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="gerar_relatorio.php">
+                <i class="fa fa-file"></i>
+                <span>Relatórios</span>
+            </a>
+        </li> 
             <?php
         }
         ?>        
         <!-- Nav Item - Pages Collapse Menu -->
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="usuarios.php">
-                <i class="fa fa-user"></i>
-                <span>Usuários</span>
-            </a>
-        </li>
-        <?php
-    }
-    ?>
-    <!-- Divider -->
-    <hr class="sidebar-divider">
-
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Gestão de Tarefas
-    </div>
-    <!-- Nav Item - Pages Collapse Menu -->
-    <?php
-        if ($usuario_logado->perfil <= 1) {
-            ?>
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="tarefas.php">
-            <i class="fa fa-tasks"></i>
-            <span>Tarefas</span>
-        </a>
-    </li>
-    <?php
-        }
-    ?>
     <li class="nav-item">
         <a class="nav-link collapsed" href="tarefas.php?filtro=equipe">
             <i class="fa fa-tasks"></i>
@@ -86,63 +56,14 @@
             <span>Minhas Tarefas</span>
         </a>
     </li>
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="gerar_relatorio.php">
-            <i class="fa fa-file"></i>
-            <span>Relatórios</span>
-        </a>
-    </li>  
+        <?php
+    }
+    ?>
 
     <!-- Divider -->
 <hr class="sidebar-divider">
 
-<!-- Heading -->
-<div class="sidebar-heading">
-    Chamados
-</div>
 
-<!-- Nav Item - Charts -->
-<li class="nav-item">
-    <a class="nav-link" href="#">
-        <i class="fa fa-window-maximize"></i>
-        <span>Meus chamados</span></a>
-</li>
-
-<!-- Nav Item - Tables -->
-<li class="nav-item">
-    <a class="nav-link" href="#">
-        <i class="fa fa-window-restore"></i>
-        <span>Novo chamado</span></a>
-</li>
-<!-- Divider -->
-<hr class="sidebar-divider">
-
-<!-- Heading -->
-<div class="sidebar-heading">
-    Atendimento
-</div>
-
-<!-- Nav Item - Charts -->
-<li class="nav-item">
-    <a class="nav-link" href="#">
-        <i class="fa fa-window-maximize"></i>
-        <span>Gerência de chamados</span></a>
-</li>
-
-<!-- Nav Item - Tables -->
-<li class="nav-item">
-    <a class="nav-link" href="#">
-        <i class="fa fa-window-restore"></i>
-        <span>Novo chamado</span></a>
-</li>
-
-<!-- Nav Item - Tables -->
-<li class="nav-item">
-    <a class="nav-link" href="#">
-        <i class="fa fa-file aria-hidden="true"></i>
-        <span>Relatórios</span></a>
-</li>
     <!-- Sidebar Toggler (Sidebar) -->
     <div class="text-center d-none d-md-inline">
         <button class="rounded-circle border-0" id="sidebarToggle"></button>
