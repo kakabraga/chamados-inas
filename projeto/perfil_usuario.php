34a394c189c7488dac6d4661663eded84fb83642
JotaDF <j.wilson.df@gmail.com>
2023-12-20 11:31:54 -0300

    Update perfil_usuario.php

--- projeto/perfil_usuario.php
+++ projeto/perfil_usuario.php
@@ -128,15 +128,15 @@ $manterPerfil = new ManterPerfil();
                                 ?>
                                 <form action="save_perfil_usuario.php" method="post">
                                     <input type="hidden" name="id" value="<?=$usuario->id ?>"/>
-                                    <img src="img/aniversario.svg" width="5%">
-                                    <span><b><?=$txt_aniversario ?></b></span>            
+                                     <img src="img/aniversario.svg" width="5%">
+                                     <span><b><?=$txt_aniversario ?></b></span>            
                                     <br/>
                                     <br/>
                                     <img src="img/email.svg" width="5%"> 
                                     <span><a class="ml-1" href="mailto:<?=$usuario->email ?>" target="_blank"><?=$usuario->email ?></a></span>
                                     <br/>
                                     <?php
-                                    if (str_trim($usuario->whatsapp) != "") {
+                                    if (trim($usuario->whatsapp) != "") {
                                     ?>
                                         <img class="neditar" src="img/whatsapp.svg" width="6%">
                                         <span class="neditar"><a href="https://api.whatsapp.com/send?phone=55<?=$txt_whatsapp ?>" target="_blank"><?=$usuario->whatsapp ?></a></span>
@@ -147,7 +147,7 @@ $manterPerfil = new ManterPerfil();
                                     <input type="text" class="editar form-control form-control-sm" style="display: none;" name="whatsapp" id="whatsapp" onkeypress="$(this).mask('(00)00000-0000');" value="<?=$usuario->whatsapp ?>"/>
                                     <br/>
                                     <?php
-                                    if (str_trim($usuario->linkedin) != "") {
+                                    if (trim($usuario->linkedin) != "") {
                                     ?>                                    
                                         <img class="neditar" src="img/linkedin.svg" width="6%">
                                         <span class="neditar"><a href="<?=$usuario->linkedin ?>" target="_blank"><?=$usuario->linkedin ?></a></span>
@@ -173,39 +173,173 @@ $manterPerfil = new ManterPerfil();
                         </div>
                     </div>
                     <!-- Call to Action-->
-                    <div class="card text-white bg-gradient-primary my-5 py-2 text-center">
-                        <p class="text-white m-0 h4">Minhas Tarefas</p>
+                    <?php
+                    if ($usuario->id === $usuario_logado->id) {
+                    ?>
+                    <div class="row">
+        <?php
+
+        include_once('actions/ManterTarefa.php');
+        $mTarefa = new ManterTarefa();
+        
+        $tarefas_responsavel = $mTarefa->listar(" WHERE t.id_responsavel=" . $usuario_logado->id);
+        $tarefas_criador = $mTarefa->listar(" WHERE t.id_criador=" . $usuario_logado->id);
+
+        if ($painel->total_responsavel > 0 || $painel->total_criador) {
+
+            $listar = isset($_REQUEST['listar']) ? $_REQUEST['listar'] : 'todas';
+            $txt_extra_titulo = "<small>(Todas)</small>";
+            switch ($listar) {
+                case 'pendentes':
+                    $txt_extra_titulo = "<small>(Pendentes)</small>";
+                    break;
+                case 'concluidas':
+                    $txt_extra_titulo = "<small>(Concluídas)</small>";
+                    break;
+            }
+            ?>
+            <!-- Project Card Example -->
+            <div class="ml-3 mr-3 card shadow mb-4 border-primary" style="width: 100%">
+                <div class="row card-header py-2 card-body bg-gradient-primary align-middle ml-0" style="width:100%">               
+                    <span class="h5 m-0 font-weight text-white"></span>
+                    <div class="col-sm ml-0" style="max-width:50px;">
+                        <i class="fa fa-tasks fa-lg text-white"></i> 
                     </div>
-                    <!-- Content Row-->
-                    <div class="row gx-4 gx-lg-5">
-                        <div class="col-md-4 mb-5">
-                            <div class="card h-100">
-                                <div class="card-body">
-                                    <h2 class="card-title">Card One</h2>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.</p>
-                                </div>
-                                <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
-                            </div>
-                        </div>
-                        <div class="col-md-4 mb-5">
-                            <div class="card h-100">
-                                <div class="card-body">
-                                    <h2 class="card-title">Card Two</h2>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod tenetur ex natus at dolorem enim! Nesciunt pariatur voluptatem sunt quam eaque, vel, non in id dolore voluptates quos eligendi labore.</p>
-                                </div>
-                                <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
-                            </div>
-                        </div>
-                        <div class="col-md-4 mb-5">
-                            <div class="card h-100">
-                                <div class="card-body">
-                                    <h2 class="card-title">Card Three</h2>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.</p>
-                                </div>
-                                <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
-                            </div>
+                    <div class="col mb-0">
+                        <span style="align:left;" class="h6 m-0 font-weight text-white">Tarefas Importantes <?= $txt_extra_titulo ?> </span>
+                    </div>
+                    <div class="col text-right" style="min-width:60%">
+                        <a class="btn btn-outline-light btn-sm" href="index.php?categoria=">  
+                            <i class="fa fa-bars" aria-hidden="true"></i> Todas &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        </a>
+
+                        <a class="btn btn-outline-light btn-sm" href="index.php?listar=pendentes&categoria=">
+                            <i class="fa fa-minus-square" aria-hidden="true"></i> Pendentes
+                        </a>
+
+                        <a class="btn btn-outline-light btn-sm" href="index.php?listar=concluidas&categoria=">
+                            <i class="fa fa-check-square" aria-hidden="true"></i> Concluídas
+                        </a>                                                         
+                    </div>
+                </div>
+                <?php
+                if ($painel->total_responsavel > 0) {
+                    ?>
+                    <div class="row ml-4 my-2 mr-3 border-bottom border-dark">
+                    <span class="float-right">
+                            <a class="btn btn-sm" data-toggle="collapse" href="#collapseTarefasAtribuidas" role="button" aria-expanded="false" aria-controls="collapseTarefasAtribuidas">
+                            <i class="fa fa-tasks fa-lg"></i> <strong> Atribuídas a mim</strong>
+                            </a>
+                        </span>
+                        
+                    </div>
+
+                    <div class="collapse show" id="collapseTarefasAtribuidas">
+                        <div class="card-body card-deck">
+                            <?php
+                            foreach ($tarefas_responsavel as $obj) {
+                                $percentual = round($mTarefa->getPercentualTarefaPorId($obj->id), 1);
+                                $islistar = true;
+                                switch ($listar) {
+                                    case 'pendentes':
+                                        $islistar = ($percentual < 100);
+                                        break;
+                                    case 'concluidas':
+                                        $islistar = ($percentual == 100);
+                                        break;
+                                }
+                                if ($islistar) {
+                                    $bg_progress = "bg-success";
+                                    if ($percentual <= 50.0) {
+                                        $bg_progress = "bg-danger";
+                                    } else if ($percentual > 50.0 && $percentual <= 99.0) {
+                                        $bg_progress = "bg-warning";
+                                    }
+                                    $txt_extra = "Início: " . date('d/m/Y', strtotime($obj->inicio)) . " Fim: " . date('d/m/Y', strtotime($obj->fim));
+                                    ?>
+                                    <div class="card text-white bg-gradient-primary mb-3" style="max-width: 18rem;">
+                                        <div class="card-header"><span class="float-right"><a href='gerenciar_etapas.php?tarefa=<?= $obj->id ?>' class='badge text-white' title='Gerenciar etapa'><i class='fa fa-cog fa-2x'></i></a></span><strong><?= $obj->nome ?></strong></div>
+                                        <div class="card-body">
+                                            <p class="card-text"><?= $obj->descricao ?> <br/><br/><?= $txt_extra ?></p>
+                                        </div>
+                                        <div class="card-footer">
+                                            <div class="progress mb-3">
+                                                <div class="progress-bar <?= $bg_progress ?>" role="progressbar" style="width: <?= $percentual ?>%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"><?= $percentual ?>%</div>   
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <?php
+                                }
+                            }
+                            ?>
                         </div>
                     </div>
+                    <?php
+                }
+                if ($painel->total_criador > 0) {
+                    ?>
+
+                    <div class="row ml-4 my-0 mr-3 border-bottom border-dark">
+                        <span class="float-right">
+                            <a class="btn btn-sm" data-toggle="collapse" href="#collapseMinhasTarefas" role="button" aria-expanded="false" aria-controls="collapseMinhasTarefas">
+                            <i class="fa fa-tasks fa-lg"></i> <strong> Minhas tarefas </strong>
+                            </a>
+                        </span>
+                        
+                    </div>
+                    <div class="collapse" id="collapseMinhasTarefas">
+                        <div class="card-body card-deck">
+                            <?php
+                            foreach ($tarefas_criador as $obj) {
+                                $percentual = round($mTarefa->getPercentualTarefaPorId($obj->id), 1);
+                                $islistar = true;
+                                switch ($listar) {
+                                    case 'pendentes':
+                                        $islistar = ($percentual < 100);
+                                        break;
+                                    case 'concluidas':
+                                        $islistar = ($percentual == 100);
+                                        break;
+                                }
+                                if ($islistar) {
+                                    $bg_progress = "bg-success";
+                                    if ($percentual <= 50.0) {
+                                        $bg_progress = "bg-danger";
+                                    } else if ($percentual > 50.0 && $percentual <= 99.0) {
+                                        $bg_progress = "bg-warning";
+                                    }
+                                    $txt_extra = "Início: " . date('d/m/Y', strtotime($obj->inicio)) . " Fim: " . date('d/m/Y', strtotime($obj->fim));
+                                    ?>
+                                    <div class="card text-white bg-gradient-primary mb-3" style="max-width: 18rem;">
+                                        <div class="card-header"><span class="float-right"><a href='gerenciar_etapas.php?tarefa=<?= $obj->id ?>' class='badge text-white' title='Gerenciar etapa'><i class='fa fa-cog fa-2x'></i></a></span><strong><?= $obj->nome ?></strong></div>
+                                        <div class="card-body">
+                                            <p class="card-text"><?= $obj->descricao ?> <br/><br/><?= $txt_extra ?></p>
+                                        </div>
+                                        <div class="card-footer">
+                                            <div class="progress mb-3">
+                                                <div class="progress-bar <?= $bg_progress ?>" role="progressbar" style="width: <?= $percentual ?>%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"><?= $percentual ?>%</div>   
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <?php
+                                }
+                            }
+                            ?>
+                        </div>
+                    </div>
+                    <?php
+                }
+                ?>
+
+            </div>
+            <?php
+        }
+        ?>
+    </div>
+                    <?php
+                    }
+                    ?>
                 </div>
             </div>
         </div>    
