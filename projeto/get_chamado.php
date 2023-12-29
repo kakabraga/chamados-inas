d355fa50d616030e9f292f3b1b163120530390ec
JotaDF <j.wilson.df@gmail.com>
2023-12-29 16:22:10 -0300

    Update get_chamado.php

--- projeto/get_chamado.php
+++ projeto/get_chamado.php
@@ -17,20 +17,20 @@ $lista = $manterChamado->listar();
 foreach ($lista as $obj) {
     $txt_setor = $manterSetor->getSetorPorId($manterUsuario->getUsuarioPorId($obj->usuario)->setor)->sigla;
     $txt_usuario = $manterUsuario->getUsuarioPorId($obj->usuario)->nome;
-    $txt_categoria = $manterCategoria->getCategoriaPorId($obj->categoria)->sigla;
-    $txt_status = '<img src="img/chamado_aberto.svg" title="Novo" width="50" />';
+    $txt_categoria = $manterCategoria->getCategoriaPorId($obj->categoria)->nome;
+    $txt_status = '<img src="img/chamado_aberto.svg" title="Novo" width="40" />';
     switch ($obj->status) {
         case 0:
-            $txt_status = '<img src="img/chamado_aberto.svg" title="Novo" width="50" />';
+            $txt_status = '<img src="img/chamado_aberto.svg" title="Novo" width="40" />';
             break;
         case 1:
-            $txt_status = '<img src="img/chamado_atendimento.svg" title="Em atendimento" width="50" />';
+            $txt_status = '<img src="img/chamado_atendimento.svg" title="Em atendimento" width="40" />';
             break;
         case 2:
-            $txt_status = '<img src="img/chamado_concluido.svg" title="Concluído" width="50" />';
+            $txt_status = '<img src="img/chamado_concluido.svg" title="Concluído" width="40" />';
             break;
         case 3:
-            $txt_status = '<img src="img/chamado_cancelado.svg" title="Cancelado" width="50" />';
+            $txt_status = '<img src="img/chamado_cancelado.svg" title="Cancelado" width="40" />';
             break;
     }
     echo "<tr>";
@@ -39,7 +39,7 @@ foreach ($lista as $obj) {
     echo "  <td>" . $txt_usuario . "</td>";
     echo "  <td>" . $txt_categoria . "</td>";
     echo "  <td>" . date('d/m/Y', strtotime($obj->data_abertura)) . "</td>";
-    echo "  <td>" . $txt_status . "</td>";
+    echo "  <td align='center'>" . $txt_status . "</td>";
     if($obj->status == 0){
         echo "  <td align='center'><button class='btn btn-primary btn-sm' type='button' onclick='atender(".$obj->id.",\"". $txt_usuario ."\",\"".$obj->descricao."\",".$obj->categoria.")' title='Atender chamado'><i class='fa fa-clock'></i></button>&nbsp;&nbsp;<button class='btn btn-danger btn-sm' type='button' onclick='cancelar(".$obj->id.",\"".$txt_usuario."\")'  title='Cancelar chamado'><i class='fa fa-ban'></i></button></td>";
     } else if($obj->status == 1){
