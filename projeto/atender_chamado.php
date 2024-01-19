4a4a4af45f0a7eff4b6c3a0d15317a282d2bd2ae
JotaDF <j.wilson.df@gmail.com>
2024-01-19 10:28:07 -0300

    Update atender_chamado.php

--- projeto/atender_chamado.php
+++ projeto/atender_chamado.php
@@ -7,11 +7,12 @@ $db_chamado = new ManterChamado();
 $c = new Chamado();
 
 $id = isset($_POST['id']) ? $_POST['id'] : 0;
+$atendente = isset($_POST['atendente']) ? $_POST['atendente'] : 0;
 $categoria = isset($_POST['categoria']) ? $_POST['categoria'] : '';
 
 $c->id = $id;
 $c->categoria = $categoria;
-
+$c->atendente = $atendente;
 $db_chamado->atender($c);
 header('Location: chamados.php');
 
