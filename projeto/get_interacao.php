5bb21e4d70b85a7cfb9228086c5027782cabb522
JotaDF <j.wilson.df@gmail.com>
2024-02-14 17:46:08 -0300

    Update get_interacao.php

--- projeto/get_interacao.php
+++ projeto/get_interacao.php
@@ -8,9 +8,19 @@
 	$lista = $manterInteracao->listar($id_chamado);
         
     foreach ($lista as $obj) {
-        echo '<div class="bd-callout bd-callout-warning">';
-        echo '<h5 id="titulo_interacao">'.$manterUsuario->getUsuarioPorId($obj->usuario)->nome.'</h5>';
-        echo '<p>'.$obj->texto.' <br/><code class="highlighter-rouge">'.$obj->data.'</code>.</p>';
-        echo '</div>';
+        ?>
+        <div class="col-xl-3 col-md-6 mb-4" style="max-width: 420px;" >
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                        <h5 id="titulo_interacao"><?=$manterUsuario->getUsuarioPorId($obj->usuario)->nome ?></h5>
+                        <p><?=$obj->texto ?><br/><code class="highlighter-rouge"><?=date('d/m/Y h:i', strtotime($obj->data)) ?></code>.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php
     }
 
