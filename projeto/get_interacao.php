3e81b6099688ba8c46dc29fc8d0f4571baed0062
JotaDF <j.wilson.df@gmail.com>
2024-02-15 08:28:25 -0300

    Update get_interacao.php

--- projeto/get_interacao.php
+++ projeto/get_interacao.php
@@ -10,16 +10,16 @@
     foreach ($lista as $obj) {
         $class_color = "border-left-primary";
         if($obj->usuario != $chamado->usuario){
-            $class_color = "border-right-warning";
+            $class_color = "border-left-warning";
         }
         ?>
-        <div class="col-xl-3 col-md-6 mb-4" style="max-width: 720px;" >
-            <div class="card <?=$class_color ?> shadow h-100 py-2">
+        <div class="col-xl-3 col-md-6 mb-2" style="max-width: 750px;" >
+            <div class="card <?=$class_color ?> shadow py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                        <h5 id="titulo_interacao"><?=$manterUsuario->getUsuarioPorId($obj->usuario)->nome ?></h5>
-                        <p><?=$obj->texto ?><br/><code class="highlighter-rouge"><?=date('d/m/Y h:i', strtotime($obj->data)) ?></code>.</p>
+                        <h6 id="titulo_interacao"><?=$manterUsuario->getUsuarioPorId($obj->usuario)->nome ?></h6>
+                        <span><?=$obj->texto ?><br/><code class="highlighter-rouge"><i><?=date('d/m/Y h:i', strtotime($obj->data)) ?></i></code>.</span>
                         </div>
                     </div>
                 </div>
