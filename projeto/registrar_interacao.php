5b54daca656c345157e18e5c66dbc2c9b9a86b88
JotaDF <j.wilson.df@gmail.com>
2024-02-21 17:11:12 -0300

    Update registrar_interacao.php

--- projeto/registrar_interacao.php
+++ projeto/registrar_interacao.php
@@ -1,15 +1,18 @@
 <?php
 
 require_once('./actions/ManterInteracao.php');
+require_once('./actions/ManterChamado.php');
 require_once('./dto/Interacao.php');
 
 $db_interacao = new ManterInteracao();
+$db_chamado = new ManterChamado();
 $i = new Interacao();
 
 $id          = isset($_POST['id']) ? $_POST['id'] : 0;
 $texto       = $_POST['texto'];
 $id_usuario  = $_POST['id_usuario'];
 $id_chamado  = $_POST['id_chamado'];
+$finalizar   = isset($_POST['finalizar']) ? $_POST['finalizar'] : 0;
 
 $i->id      =  $id;
 $i->texto   = $texto;
@@ -17,6 +20,10 @@ $i->usuario = $id_usuario;
 $i->chamado = $id_chamado;
 
 $db_interacao->salvar($i);
+
+if($finalizar > 0){
+    $db_chamado->concluir($id_chamado);
+}
 
 header('Location: gerenciar_interacoes.php?id='.$id_chamado);
 
