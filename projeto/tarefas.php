38ab79a681ed8bb6bd94181c993d15afa14312ed
JotaDF <j.wilson.df@gmail.com>
2024-02-29 11:11:20 -0300

    Update tarefas.php

--- projeto/tarefas.php
+++ projeto/tarefas.php
@@ -280,7 +280,7 @@ foreach ($listaUsuario as $obj) {
 
                     <div class="container-fluid">
                         <?php
-                        if ($usuario_logado->perfil <= 2) {
+                        if ($usuario_logado->perfil >= 1) {
                             include './form_tarefa.php';
                         }
                         ?>
@@ -296,11 +296,13 @@ foreach ($listaUsuario as $obj) {
                                 <div class="col text-right" style="max-width:20%">
                                     <?php
                                     if ($usuario_logado->perfil >= 1) {
+                                        if($filtro == 'equipe' && ($usuario_logado->perfil == 1 || $usuario_logado->perfil == 2 || $usuario_logado->perfil == 11) ){
                                         ?>
                                         <button id="btn_cadastrar" class="btn btn-outline-light btn-sm" type="button" data-toggle="collapse" data-target="#form_tarefa" aria-expanded="false" aria-controls="form_tarefa">
                                             <i class="fa fa-plus-circle text-white" aria-hidden="true"></i>
                                         </button>                            
                                         <?php
+                                        }
                                     }
                                     ?>                                    
                                 </div>
