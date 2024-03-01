c583c3c7ac5e21630853dfa676d3ed4ff6544fea
JotaDF <j.wilson.df@gmail.com>
2024-03-01 16:32:14 -0300

    Update get_aniversariantes.php

--- projeto/get_aniversariantes.php
+++ projeto/get_aniversariantes.php
@@ -6,12 +6,12 @@
     $manterSetor = new ManterSetor();
 	
 	$lista = $manterUsuario->listarAniversariantes();
-    echo '<ul class="list-group">';
+    echo '<ul class="list-group" style="width:400px;">';
     foreach ($lista as $obj) {
         
         echo '<li class="list-group-item d-flex justify-content-between align-items-center">
-            <span style="font-size: 11px;">'.$obj->nome
-            .' </span><span class="badge badge-warning badge-pill">'
+            <span style="font-size: 11px;">< href="perfil_usuario.php?id='.$obj->id.'">'.$obj->nome
+            .'</a> </span><span class="badge badge-warning badge-pill">'
             .$manterSetor->getSetorPorId($obj->setor)->sigla
             .'</span><span class="badge badge-primary badge-pill">'
             .$obj->dia.'/'.$obj->mes
