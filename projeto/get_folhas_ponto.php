42f6b268acbc34568ee5bd68555b4c53c67dea14
JotaDF <j.wilson.df@gmail.com>
2024-03-04 15:37:32 -0300

    Update get_folhas_ponto.php

--- projeto/get_folhas_ponto.php
+++ projeto/get_folhas_ponto.php
@@ -13,19 +13,22 @@
                 'Dezembro');
         
         for($i = 0; $i < count($meses);$i++) {
-            $txt_mes = "0" . $i;
-            $txt_matricula = "0" . strtoupper(str_replace("-", "", $usuario_logado->matricula));
-            $arquivo = "2024/".$txt_mes."/".$txt_matricula.".pdf";
-            echo "Arquivo: " . $arquivo;
-            echo "<hr/> " . is_file($arquivo);
-            if($i >= 10){
+            $txt_mes = "";
+            if($i > 9){
+                $txt_mes = "0" . $i;
+            } else {
                 $txt_mes = "" . $i;
             }
+            $txt_matricula = "0" . strtoupper(str_replace("-", "", $usuario_logado->matricula));
+            $arquivo = "ponto/2024/".$txt_mes."/".$txt_matricula.".pdf";
+            //echo "Arquivo: " . $arquivo;
+            //echo "<hr/> " . is_file($arquivo);
+
             echo "<tr>";
             echo "  <td>2024</td>";
             echo "  <td>".$meses[$i]."</td>";
             if(is_file($arquivo)){
-                echo "  <td align='center'><a class='btn btn-btn-info btn-sm' href='./2024/".$txt_mes."/".$txt_matricula.".pdf'><i class='fa fa-file-pdf text-white'></i></a></td>";
+                echo "  <td align='center'><a class='btn btn-btn-info btn-sm' href='./ponto/2024/".$txt_mes."/".$txt_matricula.".pdf'><i class='fa fa-file-pdf text-white'></i></a></td>";
             } else {
                 echo "  <td align='center'> - </td>";                
             }
