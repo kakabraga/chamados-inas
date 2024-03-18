1e4b3b1d2284891e8d385c7deeb84b3e1eef6fad
JotaDF <j.wilson.df@gmail.com>
2024-03-18 14:45:01 -0300

    Update get_equipe.php

--- projeto/get_equipe.php
+++ projeto/get_equipe.php
@@ -10,10 +10,14 @@
             echo "  <td>".$obj->id."</td>";
             echo "  <td>".$obj->equipe."</td>";
             echo "  <td>".$obj->descricao."</td>";
-            if($obj->excluir){
-                echo "  <td align='center'><button class='btn btn-primary btn-sm' type='button' onclick='alterar(".$obj->id.",\"".$obj->equipe."\",\"".$obj->descricao."\")'><i class='fas fa-edit'></i></button>&nbsp;&nbsp;<button class='btn btn-danger btn-sm' type='button' onclick='excluir(".$obj->id.",\"".$obj->equipe."\")'><i class='far fa-trash-alt'></i></button><a href='gerenciar_acessos.php?id=".$obj->id."' title='Gerenciar acessos' class='btn btn-warning btn-sm' type='button'><i class='fa fa-users'></i></a></td>";
+            if($usuario_logado->perfil<=2 || $usuario_logado->id == $equipe->criador){
+                if($obj->excluir){
+                    echo "  <td align='center'><button class='btn btn-primary btn-sm' type='button' onclick='alterar(".$obj->id.",\"".$obj->equipe."\",\"".$obj->descricao."\")'><i class='fas fa-edit'></i></button>&nbsp;&nbsp;<button class='btn btn-danger btn-sm' type='button' onclick='excluir(".$obj->id.",\"".$obj->equipe."\")'><i class='far fa-trash-alt'></i></button><a href='gerenciar_equipe.php?id=".$obj->id."' title='Gerenciar equipe' class='btn btn-warning btn-sm' type='button'><i class='fa fa-users'></i></a></td>";
+                } else {
+                    echo "  <td align='center'><button class='btn btn-primary btn-sm' type='button' onclick='alterar(".$obj->id.",\"".$obj->equipe."\",\"".$obj->descricao."\")'><i class='fas fa-edit'></i></button>&nbsp;&nbsp;<button class='btn btn-secondary btn-sm' type='button' title='Possuí dependências!'><i class='far fa-trash-alt' alt='Possuí dependências!'></i></button></td>";                
+                } 
             } else {
-                echo "  <td align='center'><button class='btn btn-primary btn-sm' type='button' onclick='alterar(".$obj->id.",\"".$obj->equipe."\",\"".$obj->descricao."\")'><i class='fas fa-edit'></i></button>&nbsp;&nbsp;<button class='btn btn-secondary btn-sm' type='button' title='Possuí dependências!'><i class='far fa-trash-alt' alt='Possuí dependências!'></i></button></td>";                
+                echo "<td> - </td>";
             }
             echo "</tr>";
         }
