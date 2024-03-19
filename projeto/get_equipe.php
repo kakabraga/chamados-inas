7130d334175e0255f658f87b6d500642e748e6c3
JotaDF <j.wilson.df@gmail.com>
2024-03-19 13:36:14 -0300

    Update get_equipe.php

--- projeto/get_equipe.php
+++ projeto/get_equipe.php
@@ -4,6 +4,10 @@
 	$manterEquipe = new ManterEquipe();
 	
 	$lista = $manterEquipe->listar();
+    if($usuario_logado->perfil == 2){
+        $filtro = ' WHERE criador = ' . $usuario_logado->id;
+        $lista = $manterEquipe->listar($filtro);
+    }
         
         foreach ($lista as $obj) {
             echo "<tr>";
@@ -12,9 +16,9 @@
             echo "  <td>".$obj->descricao."</td>";
             if($usuario_logado->perfil<=2 || $usuario_logado->id == $equipe->criador){
                 if($obj->excluir){
-                    echo "  <td align='center'><button class='btn btn-primary btn-sm' type='button' onclick='alterar(".$obj->id.",\"".$obj->equipe."\",\"".$obj->descricao."\")'><i class='fas fa-edit'></i></button>&nbsp;&nbsp;<button class='btn btn-danger btn-sm' type='button' onclick='excluir(".$obj->id.",\"".$obj->equipe."\")'><i class='far fa-trash-alt'></i></button><a href='gerenciar_equipe.php?id=".$obj->id."' title='Gerenciar equipe' class='btn btn-warning btn-sm' type='button'><i class='fa fa-users'></i></a></td>";
+                    echo "  <td align='center'><button class='btn btn-primary btn-sm' type='button' onclick='alterar(".$obj->id.",\"".$obj->equipe."\",\"".$obj->descricao."\")'><i class='fas fa-edit'></i></button>&nbsp;&nbsp;<button class='btn btn-danger btn-sm' type='button' onclick='excluir(".$obj->id.",\"".$obj->equipe."\")'><i class='far fa-trash-alt'></i></button>&nbsp;&nbsp;<a href='gerenciar_equipe.php?id=".$obj->id."' title='Gerenciar equipe' class='btn btn-warning btn-sm' type='button'><i class='fa fa-users'></i></a></td>";
                 } else {
-                    echo "  <td align='center'><button class='btn btn-primary btn-sm' type='button' onclick='alterar(".$obj->id.",\"".$obj->equipe."\",\"".$obj->descricao."\")'><i class='fas fa-edit'></i></button>&nbsp;&nbsp;<button class='btn btn-secondary btn-sm' type='button' title='Possuí dependências!'><i class='far fa-trash-alt' alt='Possuí dependências!'></i></button></td>";                
+                    echo "  <td align='center'><button class='btn btn-primary btn-sm' type='button' onclick='alterar(".$obj->id.",\"".$obj->equipe."\",\"".$obj->descricao."\")'><i class='fas fa-edit'></i></button>&nbsp;&nbsp;<button class='btn btn-secondary btn-sm' type='button' title='Possuí dependências!'><i class='far fa-trash-alt' alt='Possuí dependências!'></i></button>&nbsp;&nbsp;<a href='gerenciar_equipe.php?id=".$obj->id."' title='Gerenciar equipe' class='btn btn-warning btn-sm' type='button'><i class='fa fa-users'></i></a></td>";                
                 } 
             } else {
                 echo "<td> - </td>";
