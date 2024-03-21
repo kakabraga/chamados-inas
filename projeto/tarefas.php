65bf32923e1c0067e088be768145ac879e9d94ce
JotaDF <j.wilson.df@gmail.com>
2024-03-21 17:11:55 -0300

    Update tarefas.php

--- projeto/tarefas.php
+++ projeto/tarefas.php
@@ -81,12 +81,12 @@ $titulo = 'Tarefas';
 switch ($pesquisa) {
     case 'equipe':
         $equipesUsuario  = $manterUsuario->getEquipesUsuarioParticipante($usuario_logado->id);
-        $filtro_equipes = "0";
+        $filtro_equipes = "";
         $txt_equipes = "";
         foreach ($equipesUsuario as $eq) {
             if ($txt_equipes === "") {
-                $filtro_equipes = ", ". $eq->id;
-                $txt_equipes = $eq->equipe;
+                $filtro_equipes .=  $eq->id;
+                $txt_equipes .= $eq->equipe;
             } else {
                 $filtro_equipes .= ", ". $eq->id;
                 $txt_equipes .= ", ". $eq->equipe;
