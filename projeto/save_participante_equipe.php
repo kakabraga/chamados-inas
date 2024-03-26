66070c5b8ea93a9c42933f6b2eebd92f4eb4b15c
JotaDF <j.wilson.df@gmail.com>
2024-03-25 22:28:33 -0300

    Update save_participante_equipe.php

--- projeto/save_participante_equipe.php
+++ projeto/save_participante_equipe.php
@@ -1,6 +1,11 @@
 <?php
 
 include_once('actions/ManterEquipe.php');
+require_once('./actions/ManterNotificacao.php');
+require_once('./dto/Notificacao.php');
+
+$db_notificacao = new ManterNotificacao();
+$n = new Notificacao();
 
 $manterEquipe = new ManterEquipe();
 
@@ -8,10 +13,27 @@ $id_equipe  = $_REQUEST['id_equipe'];
 $id_usuario = $_REQUEST['id_usuario'];
 $op = isset($_REQUEST['op']) ? $_REQUEST['op'] : 1;
 
+
 if ($op == 1) {
     $manterEquipe->add($id_equipe,$id_usuario);
-    header('Location: gerenciar_equipes.php?id='.$id_equipe);
+
+    // Registrando notificação
+    $n->texto   = "Você foi adicionado a uma equipe!";
+    $n->usuario = $id_usuario;
+    $n->link = 'gerenciar_equipe.php?id=' . $id_equipe;
+    $n->tipo = 'equipe';
+    $db_notificacao->salvar($n);
+
+    header('Location: gerenciar_equipe.php?id='.$id_equipe);
 } else {
     $manterEquipe->del($id_equipe,$id_usuario);
-    header('Location: gerenciar_equipes.php?id='.$id_equipe);
+
+    // Registrando notificação
+    $n->texto   = "Você foi removido de uma equipe!";
+    $n->usuario = $id_usuario;
+    $n->link = 'gerenciar_equipe.php?id=' . $id_equipe;
+    $n->tipo = 'equipe';
+    $db_notificacao->salvar($n);
+
+    header('Location: gerenciar_equipe.php?id='.$id_equipe);
 }
