39ed3c6de06bf578acb612d99a1560fd5c074249
JotaDF <j.wilson.df@gmail.com>
2024-04-01 17:56:10 -0300

    Update form_processo.php

--- projeto/form_processo.php
+++ projeto/form_processo.php
@@ -1,14 +1,14 @@
 <!-- Begin Page Content -->
 
 <!-- Collapsable Form -->
-<div class="card mb-4 collapse hide border-primary" id="form_assunto" style="max-width:900px">
+<div class="card mb-4 collapse hide border-primary" id="form_processo" style="max-width:900px">
     <!-- Card Header - Accordion -->
     <div class="card-header py-2 card-body bg-gradient-primary align-middle" style="min-height: 2.5rem;">               
         <span class="h6 m-0 font-weight text-white">Cadastro de processo</span>
     </div>                  
     <!-- Card Content - Collapse -->
     <div class="card-body">
-        <form id="form_cadastro" action="save_assunto.php" method="post">
+        <form id="form_cadastro" action="save_processo.php" method="post">
             <input type="hidden" id="id" name="id"/>
             <div class="form-row">
                 <div class="form-group col-md-6">
@@ -47,7 +47,7 @@
             </div>
 
             <div class="form-group row float-right">
-                <button type="reset" data-toggle="collapse" data-target="#form_assunto" class="btn btn-danger btn-sm"><i class="fa fa-minus-square"></i> Cancelar</button>
+                <button type="reset" data-toggle="collapse" data-target="#form_processo" class="btn btn-danger btn-sm"><i class="fa fa-minus-square"></i> Cancelar</button>
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 <button type="submit" class="btn btn-primary btn-sm"><i class="fas fa-save"></i> Salvar</button>
                 &nbsp;&nbsp;&nbsp;
