3efafb83a49d7132c907cc4f2a1f43abfb2974fa
JotaDF <j.wilson.df@gmail.com>
2024-04-01 21:58:27 -0300

    Update form_processo.php

--- projeto/form_processo.php
+++ projeto/form_processo.php
@@ -11,7 +11,7 @@
         <form id="form_cadastro" action="save_processo.php" method="post">
             <input type="hidden" id="id" name="id"/>
             <div class="form-row">
-                <div class="form-group col-md-6">
+                <div class="form-group col-md-4">
                 <label for="numero">Número</label>
                 <input type="text" class="form-control form-control-sm" id="numero" placeholder="00000">
                 </div>
@@ -19,37 +19,67 @@
                 <label for="sei">SEI</label>
                 <input type="text" class="form-control form-control-sm" id="sei" placeholder="0000">
                 </div>
-                <div class="form-group col-md-2">
+                <div class="form-group col-md-4">
                 <label for="autuacao">Autuação</label>
                 <input type="date" class="form-control form-control-sm" id="autuacao">
                 </div>
             </div>
             <div class="form-group">
-                <label for="inputAddress">Endereço</label>
-                <input type="text" class="form-control form-control-sm" id="inputAddress" placeholder="Rua dos Bobos, nº 0">
+                <div class="form-group col-md-4">
+                <label for="cpf">CPF</label>
+                <input type="text" class="form-control form-control-sm" id="cpf" placeholder="000000000-00">
+                </div>
+                <div class="form-group col-md-6">
+                <label for="benaficiario">Beneficiário</label>
+                <input type="text" class="form-control form-control-sm" id="benaficiario" placeholder="nome">
+                </div>           
             </div>
             <div class="form-group">
-                <label for="inputAddress2">Endereço 2</label>
-                <input type="text" class="form-control form-control-sm" id="inputAddress2" placeholder="Apartamento, hotel, casa, etc.">
-            </div>
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                <label for="inputCity">Cidade</label>
-                <input type="text" class="form-control form-control-sm" id="inputCity">
-                </div>
                 <div class="form-group col-md-4">
-                <label for="inputEstado">Estado</label>
-                <select id="inputEstado" class="form-control form-control-sm">
-                    <option selected>Escolher...</option>
-                    <option>...</option>
-                </select>
+                <label for="assunto">Assunto</label>
+                <input type="text" class="form-control form-control-sm" id="assunto" placeholder="Assunto">
+                </div>
+                <div class="form-group col-md-6">
+                <label for="senha">Senha</label>
+                <input type="text" class="form-control form-control-sm" id="senha" placeholder="senha">
+                </div>            
+            </div>
+            <div class="form-group">
+                <div class="form-group col-md-2">
+                <label for="valor_guia">Valor da Guia</label>
+                <input type="text" class="form-control form-control-sm" id="valor_guia" placeholder="000,00">
                 </div>
                 <div class="form-group col-md-2">
-                <label for="inputCEP">CEP</label>
-                <input type="text" class="form-control form-control-sm" id="inputCEP">
+                <label for="valor_causa">Valor da Causa</label>
+                <input type="text" class="form-control form-control-sm" id="valor_causa" placeholder="000,00">
+                </div> 
+                <div class="form-group col-md-2">
+                <label for="deposito_judicial">Depósito Judicial</label>
+                <input type="text" class="form-control form-control-sm" id="deposito_judicial" placeholder="000,00">
                 </div>
+                <div class="form-group col-md-2">
+                <label for="reembolso">Reembolso</label>
+                <input type="text" class="form-control form-control-sm" id="reembolso" placeholder="000,00">
+                </div>            
             </div>
-
+            <div class="form-group">
+                <div class="form-group col-md-2">
+                <label for="custas">Custas</label>
+                <input type="text" class="form-control form-control-sm" id="custas" placeholder="000,00">
+                </div>
+                <div class="form-group col-md-2">
+                <label for="honorarios">Honorários</label>
+                <input type="text" class="form-control form-control-sm" id="honorarios" placeholder="000,00">
+                </div> 
+                <div class="form-group col-md-2">
+                <label for="multa">Multa</label>
+                <input type="text" class="form-control form-control-sm" id="multa" placeholder="000,00">
+                </div>
+                <div class="form-group col-md-2">
+                <label for="danos_morais">Danos Morais</label>
+                <input type="text" class="form-control form-control-sm" id="danos_morais" placeholder="000,00">
+                </div>            
+            </div>
             <div class="form-group row float-right">
                 <button type="reset" data-toggle="collapse" data-target="#form_processo" class="btn btn-danger btn-sm"><i class="fa fa-minus-square"></i> Cancelar</button>
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
