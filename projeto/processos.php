cc1e258ccfbbf4f573a6a5832c9a638e4d683726
JotaDF <j.wilson.df@gmail.com>
2024-04-03 18:04:26 -0300

    Update processos.php

--- projeto/processos.php
+++ projeto/processos.php
@@ -201,7 +201,23 @@ foreach ($listaCJ as $obj) {
                     html += '<option value="' + option.id + '" ' + selected + '>' + option.classe + '</option>';
                 }
                 $('#classe_judicial').html(html);
-            }        
+            } 
+            const mascaraMoeda = (event) => {
+            const onlyDigits = event.target.value
+                .split("")
+                .filter(s => /\d/.test(s))
+                .join("")
+                .padStart(3, "0")
+            const digitsFloat = onlyDigits.slice(0, -2) + "." + onlyDigits.slice(-2)
+            event.target.value = maskCurrency(digitsFloat)
+            }
+
+            const maskCurrency = (valor, locale = 'pt-BR', currency = 'BRL') => {
+            return new Intl.NumberFormat(locale, {
+                style: 'currency',
+                currency
+            }).format(valor)
+            }       
         </script>
         <style>
             body{
