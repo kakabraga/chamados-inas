6cd0e0529b1201faec4a255dce0ab4dde4b4ed48
JotaDF <j.wilson.df@gmail.com>
2024-04-09 16:09:11 -0300

    Update form_recepcao.php

--- projeto/form_recepcao.php
+++ projeto/form_recepcao.php
@@ -9,45 +9,46 @@
     <!-- Card Content - Collapse -->
     <div class="card-body">
         <form id="form_cadastro" action="save_recepcao.php" method="post">
-            <input type="hidden" id="id" name="id"/>
             <input type="hidden" id="usuario" name="usuario" value="<?=$usuario_logado->id ?>"/>
+            <input type="hidden" id="id" name="id"/>
             <div class="form-row">
-                <div class="col-md-5">
+                <div class="form-group col-md-5">
                     <label for="visitante" class="col-form-label">Visitante:</label>
                     <input type="text" name="visitante" class="form-control form-control-sm" id="visitante" placeholder="Visitante" required>
                 </div>
-                <div class="col-md-5">
+                <div class="form-group col-md-5">
                     <label for="empresa" class="col-form-label">Órgão/Empresa:</label>
                     <input type="text" name="empresa" class="form-control form-control-sm" id="empresa" placeholder="Órgão/Empresa" required>
-                </div>
+                </div>                
             </div>
             <div class="form-row">
-                <div class="col-md-5">
+                <div class="form-group col-md-5">
                     <label for="setor" class="col-form-label">Setor:</label>
-                        <input type="text" name="setor" class="form-control form-control-sm" id="setor" placeholder="Setor" required>
+                    <input type="text" name="setor" class="form-control form-control-sm" id="setor" placeholder="Setor" required>
                 </div>
-                <div class="ml-2">
+                <div class="form-group col-md-5">
                     <label for="horario" class="col-form-label">Horário:</label>
-                    <input type="text" name="horario" onkeypress="$(this).mask('00:00');" class="form-control form-control-sm" id="horario" placeholder>                </div>
-                </div>
+                    <input type="text" name="horario" onkeypress="$(this).mask('00:00');" class="form-control form-control-sm" id="horario" placeholder="00:00" required>
+                </div>                
             </div>
             <div class="form-row">
-                <div class="col-md-5">
+                <div class="form-group col-md-5">
                     <label for="recebido_por" class="col-form-label">Recebido por:</label>
                     <input type="text" name="recebido_por" class="form-control form-control-sm" id="recebido_por" placeholder="Recebido por" required>
                 </div>
-                <div class="col-md-5">
+                <div class="form-group col-md-5">
                     <label for="assunto" class="col-form-label">Assunto:</label>
                     <input type="text" name="assunto" class="form-control form-control-sm" id="assunto" placeholder="Assunto" required>
-                </div>
+                </div>                
             </div>
-            <div class="form-row float-right mt-2">
+                      
+            <div class="form-group row float-right">
                 <button type="reset" data-toggle="collapse" data-target="#form_recepcao" class="btn btn-danger btn-sm"><i class="fa fa-minus-square"></i> Cancelar</button>
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <button type="submit" class="btn btn-primary btn-sm"><i class="fas fa-save"></i> Salvar</button>
+                <button type="submit" class="btn btn-primary btn-sm mr-1"><i class="fas fa-save"></i> Salvar</button>
                 &nbsp;&nbsp;&nbsp;
             </div>
-        </form>                  
+        </form>                 
     </div>
 </div>
 <!-- /.container-fluid -->
