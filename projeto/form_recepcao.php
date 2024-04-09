196a5e4708c56cb01e1b99fd1c62b91dfab6d248
JotaDF <j.wilson.df@gmail.com>
2024-04-09 15:01:14 -0300

    Update form_recepcao.php

--- projeto/form_recepcao.php
+++ projeto/form_recepcao.php
@@ -12,55 +12,37 @@
             <input type="hidden" id="id" name="id"/>
             <input type="hidden" id="usuario" name="usuario" value="<?=$usuario_logado->id ?>"/>
             <div class="form-row">
-                <div class="form-group col-md-4">
+                <div class="col-md-5">
                     <label for="visitante" class="col-sm-2 col-form-label">Visitante:</label>
-                    <div class="col-sm-10">
-                        <input type="text" name="visitante" class="form-control form-control-sm" id="visitante" placeholder="Visitante" required>
-                    </div>
+                    <input type="text" name="visitante" class="form-control form-control-sm" id="visitante" placeholder="Visitante" required>
                 </div>
-            </div>
-            <div class="form-row">
-                <div class="form-group col-md-4">
+                <div class="col-md-5">
                     <label for="empresa" class="col-sm-2 col-form-label">Órgão/Empresa:</label>
-                    <div class="col-sm-10">
-                        <input type="text" name="empresa" class="form-control form-control-sm" id="empresa" placeholder="Órgão/Empresa" required>
-                    </div>
+                    <input type="text" name="empresa" class="form-control form-control-sm" id="empresa" placeholder="Órgão/Empresa" required>
                 </div>
             </div>
             <div class="form-row">
-                <div class="form-group col-md-4">
-                    <label for="setor" class="col-sm-2 col-form-label">Setor:</label>
-                    <div class="col-sm-10">
+                <div class="col-md-5">
+                    <label for="setor" class="col-form-label">Setor:</label>
                         <input type="text" name="setor" class="form-control form-control-sm" id="setor" placeholder="Setor" required>
-                    </div>
                 </div>
-                <div class="form-group col-md-4">
-                    <label for="horario" class="col-sm-2 col-form-label">Horário:</label>
-                    <div class="col-sm-10">
-                        <input type="text" name="horario" onkeypress="$(this).mask('00:00');" class="form-control form-control-sm" id="horario" placeholder="00:00" required>
-                    </div>
+                <div class="ml-2">
+                    <label for="horario" class="col-form-label">Horário:</label>
+                    <input type="text" name="horario" onkeypress="$(this).mask('00:00');" class="form-control form-control-sm" id="horario" placeholder>                </div>
                 </div>
-            </div>
             </div>
             <div class="form-row">
-                <div class="form-group col-md-4">
+                <div class="col-md-5">
                     <label for="recebido_por" class="col-sm-2 col-form-label">Recebido por:</label>
-                    <div class="col-sm-10">
-                        <input type="text" name="recebido_por" class="form-control form-control-sm" id="recebido_por" placeholder="Recebido por" required>
-                    </div>
+                    <input type="text" name="recebido_por" class="form-control form-control-sm" id="recebido_por" placeholder="Recebido por" required>
                 </div>
-            </div>
-            <div class="form-row">
-                <div class="form-group col-md-4">
+                <div class="col-md-5">
                     <label for="assunto" class="col-sm-2 col-form-label">Assunto:</label>
-                    <div class="col-sm-10">
-                        <input type="text" name="assunto" class="form-control form-control-sm" id="assunto" placeholder="Assunto" required>
-                    </div>
+                    <input type="text" name="assunto" class="form-control form-control-sm" id="assunto" placeholder="Assunto" required>
                 </div>
             </div>
-            <div class="form-group row float-right">
-                <button type="reset" data-toggle="collapse" data-target="#form_recepcao" class="btn btn-danger btn-sm"><i class="fa fa-minus-square"></i> Cancelar</button>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <div class="form-row float-right mt-2">
+                <button type="reset" data-toggle="collapse" data-target="#form_recepcao" class="btn btn-danger btn-sm"><i class="fa fa-minus-square"></i> C>                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 <button type="submit" class="btn btn-primary btn-sm"><i class="fas fa-save"></i> Salvar</button>
                 &nbsp;&nbsp;&nbsp;
             </div>
