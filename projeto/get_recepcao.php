0bb78c21e25112c953030b6c96c6d29cfae6af0c
JotaDF <j.wilson.df@gmail.com>
2024-04-10 08:48:31 -0300

    Update get_recepcao.php

--- projeto/get_recepcao.php
+++ projeto/get_recepcao.php
@@ -1,4 +1,6 @@
 <?php
+    date_default_timezone_set('America/Sao_Paulo');
+
 	include_once('actions/ManterRecepcao.php'); 
 	
 	$manterRecepcao = new ManterRecepcao();
@@ -12,10 +14,16 @@
             echo "  <td>".$obj->empresa."</td>";
             echo "  <td>".$obj->setor."</td>";
             echo "  <td>".$obj->horario."</td>";
-            if($obj->excluir){
-                echo "  <td align='center'><button class='btn btn-primary btn-sm' type='button' onclick='alterar(".$obj->id.",\"".$obj->visitante."\",\"".$obj->empresa."\",\"".$obj->horario."\",\"".$obj->setor."\",\"".$obj->recebido_por."\",\"".$obj->assunto."\")'><i class='fas fa-edit'></i></button>&nbsp;&nbsp;<button class='btn btn-danger btn-sm' type='button' onclick='excluir(".$obj->id.",\"".$obj->visitante."\")'><i class='far fa-trash-alt'></i></button></td>";
+            $hoje = date('d/m/Y');
+            $data_cadastro = date('d/m/Y', strtotime($obj->cadastro));
+            if($hoje == $data_cadastro || $usuario_logado->perfil <=2){
+                if($obj->excluir){
+                    echo "  <td align='center'><button class='btn btn-primary btn-sm' type='button' onclick='alterar(".$obj->id.",\"".$obj->visitante."\",\"".$obj->empresa."\",\"".$obj->horario."\",\"".$obj->setor."\",\"".$obj->recebido_por."\",\"".$obj->assunto."\")'><i class='fas fa-edit'></i></button>&nbsp;&nbsp;<button class='btn btn-danger btn-sm' type='button' onclick='excluir(".$obj->id.",\"".$obj->visitante."\")'><i class='far fa-trash-alt'></i></button></td>";
+                } else {
+                    echo "  <td align='center'><button class='btn btn-primary btn-sm' type='button' onclick='alterar(".$obj->id.",\"".$obj->visitante."\",\"".$obj->empresa."\",\"".$obj->horario."\",\"".$obj->setor."\",\"".$obj->recebido_por."\",\"".$obj->assunto."\")'><i class='fas fa-edit'></i></button>&nbsp;&nbsp;<button class='btn btn-secondary btn-sm' type='button' title='Possuí dependências!'><i class='far fa-trash-alt' alt='Possuí dependências!'></i></button></td>";                
+                }
             } else {
-                echo "  <td align='center'><button class='btn btn-primary btn-sm' type='button' onclick='alterar(".$obj->id.",\"".$obj->visitante."\",\"".$obj->empresa."\",\"".$obj->horario."\",\"".$obj->setor."\",\"".$obj->recebido_por."\",\"".$obj->assunto."\")'><i class='fas fa-edit'></i></button>&nbsp;&nbsp;<button class='btn btn-secondary btn-sm' type='button' title='Possuí dependências!'><i class='far fa-trash-alt' alt='Possuí dependências!'></i></button></td>";                
+                echo "  <td align='center'> - </td>";
             }
             echo "</tr>";
         }
