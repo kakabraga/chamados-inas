0a4a68ba06816a986a59bf6fdc30d177540a2a03
JotaDF <j.wilson.df@gmail.com>
2024-04-11 13:44:21 -0300

    Create agenda.php

---
+++ projeto/agenda.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang='pt-br'>
+  <head>
+    <meta charset='utf-8' />
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.11/index.global.min.js'></script>
+    <script>
+
+      document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth'
+        });
+        calendar.render();
+      });
+
+    </script>
+    <style>
+     body {
+        margin: 40px 10px;
+        padding: 0;
+        font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+        font-size: 14px;
+        }
+
+        #calendar {
+        max-width: 1100px;
+        margin: 0 auto;
+        }
+
+    </style>
+  </head>
+  <body>
+    <div id='calendar'></div>
+  </body>
+</html>
