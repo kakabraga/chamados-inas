187eafa462bb346316db1d1de246e63976c04000
JotaDF <j.wilson.df@gmail.com>
2024-04-11 13:50:09 -0300

    agenda

--- projeto/agenda.php
+++ projeto/agenda.php
@@ -2,7 +2,7 @@
 <html lang='pt-br'>
   <head>
     <meta charset='utf-8' />
-    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.11/index.global.min.js'></script>
+    <script src='js/index.global.min.js'></script>
     <script>
 
       document.addEventListener('DOMContentLoaded', function() {
