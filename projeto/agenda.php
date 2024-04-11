80eea7761b95455abc5af8b8faea9149305b6c69
JotaDF <j.wilson.df@gmail.com>
2024-04-11 15:51:20 -0300

    comit

--- projeto/agenda.php
+++ projeto/agenda.php
@@ -2,22 +2,23 @@
 <html lang='pt-br'>
   <head>
     <meta charset='utf-8' />
-    <link href='https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/css/bootstrap.css' rel='stylesheet'>
-    <link href='https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.13.1/css/all.css' rel='stylesheet'>
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'>
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css' rel='stylesheet'>
     <script src='js/index.global.min.js'></script>
     <script src='js/core/locales-all.global.min.js'></script>
+    <script src='js/core/bootstrap5/index.global.min.js'></script>
     <script>
   document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
+     themeSystem: 'bootstrap5',
       headerToolbar: {
         left: 'prev,next today',
         center: 'title',
         right: 'dayGridMonth,timeGridWeek,timeGridDay'
       },
       locale: 'pt-br',
-      themeSystem: 'bootstrap',
       initialDate: '2023-01-12',
       navLinks: true, // can click day/week names to navigate views
       selectable: false,
