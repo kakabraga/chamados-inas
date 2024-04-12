4a7a4d7aac6d922e9b5d851c17a6a3aff56bb44e
JotaDF <j.wilson.df@gmail.com>
2024-04-12 11:45:31 -0300

    Update agenda.php

--- projeto/agenda.php
+++ projeto/agenda.php
@@ -4,6 +4,7 @@
     <meta charset='utf-8' />
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'>
     <link href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css' rel='stylesheet'>
+    <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
     <script src='js/index.global.min.js'></script>
     <script src='js/core/locales-all.global.min.js'></script>
     <script src='js/core/bootstrap5/index.global.min.js'></script>
