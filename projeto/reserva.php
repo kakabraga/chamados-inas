6baed1c0bb7dd8773b4526740e262dc5880e09ce
JotaDF <j.wilson.df@gmail.com>
2024-04-15 11:42:12 -0300

    Create reserva.php

---
+++ projeto/reserva.php
@@ -0,0 +1,171 @@
+<?php
+//Recepção
+$mod = 7;
+require_once('./verifica_login.php');
+?> 
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Reserva - INAS</title>
+
+        <!-- Custom fonts for this template-->
+        <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+        <!-- Custom styles for this template-->
+        <link href="css/sb-admin-2.min.css" rel="stylesheet">
+        <link rel="shortcut icon" href="favicon.ico" />
+        <!------ Include the above in your HEAD tag ---------->
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
+        <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
+        <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'>
+        <link href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css' rel='stylesheet'>
+        <script src='js/index.global.min.js'></script>
+        <script src='js/core/locales-all.global.min.js'></script>
+        <script src='js/core/bootstrap5/index.global.min.js'></script>
+
+        <script type="text/javascript" class="init">
+         
+                document.addEventListener('DOMContentLoaded', function() {
+                var calendarEl = document.getElementById('calendar');
+
+                var calendar = new FullCalendar.Calendar(calendarEl, {
+                themeSystem: 'bootstrap5',
+                headerToolbar: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                },
+                locale: 'pt-br',
+                //initialDate: '2023-01-12',
+                navLinks: true, // can click day/week names to navigate views
+                selectable: false,
+                selectMirror: false,
+                select: function(arg) {
+                    var title = prompt('Event Title:');
+                    if (title) {
+                    calendar.addEvent({
+                        title: title,
+                        start: arg.start,
+                        end: arg.end,
+                        allDay: arg.allDay
+                    })
+                    }
+                    calendar.unselect()
+                },
+                eventClick: function(info) {
+                    const reserva = new bootstrap.Modal(document.getElementById("reserva"));
+                    reserva.show();
+                },
+                editable: false,
+                dayMaxEvents: true, // allow "more" link when too many events
+                events: 'get_reserva.php'
+
+                });
+
+                calendar.render();
+            });
+
+            $(document).ready(function () {
+            });
+            function excluir(id, visitante) {
+                $('#delete').attr('href', 'del_recepcao.php?id=' + id);
+                $('#excluir').text(visitante);
+                $('#confirm').modal({show: true});              
+            }
+            function alterar(id, visitante, empresa, horario, setor, recebido_por, assunto) {
+                $('#id').val(id);
+                $('#visitante').val(visitante);
+                $('#empresa').val(empresa);
+                $('#horario').val(horario);
+                $('#setor').val(setor);
+                $('#recebido_por').val(recebido_por);
+                $('#assunto').val(assunto);
+                $('#form_recepcao').collapse("show");
+                $('#btn_cadastrar').hide();
+            }
+
+
+        </script>
+        <style>
+            body {
+        margin: 40px 10px;
+        padding: 0;
+        font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+        font-size: small;
+        }
+
+        #calendar {
+        max-width: 1100px;
+        margin: 0 auto;
+        }
+        </style>
+    </head>
+
+    <body id="page-top">
+
+        <!-- Page Wrapper -->
+        <div id="wrapper">
+            <?php include './menu_recepcao.php'; ?>
+            <!-- Content Wrapper -->
+            <div id="content-wrapper" class="d-flex flex-column">
+                <!-- Main Content -->
+                <div id="content">
+                    <?php include './top_bar.php'; ?>
+
+                    <div class="container-fluid">
+                    <div id='calendar'></div>
+                    </div>
+                    <!-- End of Main Content -->
+                </div> 
+                <?php include './rodape.php'; ?>
+
+            </div>
+            <!-- End of Content Wrapper -->
+
+        </div>
+        <!-- End of Page Wrapper -->
+
+        <!-- Scroll to Top Button-->
+        <a class="scroll-to-top rounded" href="#page-top">
+            <i class="fas fa-angle-up"></i>
+        </a>
+        <!-- Modal excluir -->
+        <div class="modal fade" id="confirm" role="dialog">
+            <div class="modal-dialog modal-sm">
+
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Confirmação</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <p>Deseja excluir <strong>"<span id="excluir"></span>"</strong>?</p>
+                    </div>
+                    <div class="modal-footer">
+                        <a href="#" type="button" class="btn btn-danger" id="delete">Excluir</a>
+                        <button type="button" data-dismiss="modal" class="btn btn-secondary">Cancelar</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </body>
+
+</html>
