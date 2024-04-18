82259ef8ac1d157983a454a755497d18f94077ae
JotaDF <j.wilson.df@gmail.com>
2024-04-18 16:30:11 -0300

    Update save_agenda.php

--- projeto/save_agenda.php
+++ projeto/save_agenda.php
@@ -6,14 +6,19 @@ require_once('./dto/Agenda.php');
 $db_agenda = new ManterAgenda();
 $a = new Agenda();
 
-$a->id = isset($_POST['id']) ? $_POST['id'] : 0;
-$a->titulo = $_POST['titulo'];
-$a->descricao = $_POST['descricao'];
-$a->cor = $_POST['cor'];
-$a->inicio = date('Y/m/d H:i:s', strtotime($_POST['inicio']));
-$a->termino = date('Y/m/d H:i:s', strtotime($_POST['termino']));
-$a->usuario = $_POST['id_usuario'];
+if(isset($_POST['delete'])){
+    $id = isset($_POST['id']) ? $_POST['id'] : 0;
+    $db_agenda->excluir($id);
+} else { 
+    $a->id = isset($_POST['id']) ? $_POST['id'] : 0;
+    $a->titulo = $_POST['titulo'];
+    $a->descricao = $_POST['descricao'];
+    $a->cor = $_POST['cor'];
+    $a->inicio = date('Y/m/d H:i:s', strtotime($_POST['inicio']));
+    $a->termino = date('Y/m/d H:i:s', strtotime($_POST['termino']));
+    $a->usuario = $_POST['id_usuario'];
+    $db_agenda->salvar($a);
+}
 
-$db_agenda->salvar($a);
 header('Location: agenda.php');
 
